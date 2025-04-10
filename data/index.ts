f54813870d80ac958da4650b9ed20f28eb9e2ee2
antwi robert <roberttantwi@outlook.com>
2025-04-10 15:18:50 +0000

    Initial commit with project files

---
+++ data/index.ts
@@ -0,0 +1,168 @@
+export const  navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Xora AI Video Editor",
+    des: "I designed Xora AI Video Editor to automate video editing tasks, reducing editing time by 70%. With features like real-time collaboration, secure cloud storage, and flexible pricing, Xora helps creators and teams work smarter, not harder, by simplifying the editing process and boosting productivity.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/js.png", "/fm.svg", "/three.svg"],
+    link: "https://ra-xora-saas-landing-page.vercel.app",
+  },
+  {
+    id: 2,
+    title: "Animated Apple Iphone 3D Website",
+    des: "I recreated the Apple iPhone 15 Pro website using GSAP animations and Three.js 3D effects, enhancing user engagement by 40%. The immersive 3D experience and smooth animations captivate visitors, improving interaction and increasing time spent on the site, while showcasing modern web development techniques.",
+    img: "/p2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://ra-apple-iphone.vercel.app",
+  },
+  {
+    id: 3,
+    title: "Brainwave - AI Chat App",
+    des: "I developed Brainwave, an AI-powered chat app that automates conversations and provides personalized recommendations, improving productivity by 50%. With seamless integration across multiple platforms, Brainwave’s smart automation and advanced AI capabilities help users quickly find answers and enhance collaboration, all while ensuring top-notch security. This app is designed to make information retrieval and task management easier for both individuals and teams, streamlining daily operations.",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/js.png", "/fm.svg", "/three.svg"],
+    link: "https://ra-brainwave.vercel.app",
+  },
+  {
+    id: 4,
+    title: "MomenumFlow - Streamline Your Workflow",
+    des: "I developed MomentumFlow to help users track progress and stay motivated by offering a dynamic, responsive SaaS platform. By integrating Framer Motion animations, the app offers smooth transitions and interactive UI elements that enhance the user experience. The app enables teams to manage tasks efficiently, with seamless integrations, unlimited projects, and customizable workflows, boosting productivity and collaboration for teams of all sizes. With flexible pricing and real-time updates, MomentumFlow is designed to keep users on track toward their goals.",
+    img: "/p4.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/fm.svg", "/three.svg"],
+    link: "https://ra-momentumflow.vercel.app",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Robert was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Robert's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Robert is the ideal partner.",
+    name: "John Dohsas",
+    title: "Project Manager at UrbanTech",
+    img: "/profile.png"
+  },
+  {
+    quote:
+      "Working with Robert was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+    img: "/profile.svg"
+  },
+  {
+    quote:
+      "Collaborating with Robert was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Robert's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Robert is the ideal partner.",
+    name: "John Dohsas",
+    title: "Project Manager at UrbanTech",
+    img: "/profile.png"
+  },
+  {
+    quote:
+      "Working with Robert was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+    img: "/profile.svg"
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Improved page load speed by 40% by optimizing React components and lazy-loading assets, resulting in a smoother user experience and a 20% increase in user retention.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Freelance Web App Dev Project",
+    desc: "Increased user engagement by 45% by redesigning the UX with a mobile-first approach and interactive UI animations using Tailwind and React.",
+    className: "md:col-span-2", 
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
